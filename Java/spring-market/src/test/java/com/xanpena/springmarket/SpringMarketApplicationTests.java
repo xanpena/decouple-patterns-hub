2e1f2c1c7c5b7a245fa1b54ee944928fca241c51
Xan Pena <xanpena@outlook.es>
2023-12-13 13:07:29 +0100

    Initializr https://start.spring.io/

---
+++ Java/spring-market/src/test/java/com/xanpena/springmarket/SpringMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xanpena.springmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
